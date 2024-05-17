52ae72886c570ba70db9b7aa6d324e987fa2d81d
Miguel Muñoz <Miguel Muoz>
2024-05-17 14:03:42 -0500

    Implemetación de la función itinerariosEscalas añadida

--- src/main/scala/Itinerarios/package.scala
+++ src/main/scala/Itinerarios/package.scala
@@ -29,17 +29,27 @@ package object Itinerarios {
     (a: String, b: String) => List()
   }
 
-  def itinerariosEscalas(
-      vuelos: List[Vuelo],
-      aeropuertos: List[Aeropuerto]
-  ): (String, String) => List[Itinerario] = {
-    // Recibe vuelos, una lista de todos los vuelos disponibles y
-    // aeropuertos, una lista de todos los aeropuertos
-    // y devuelve una función que recibe c1 y c2, códigos de aeropuerto
-    // y devuelve los tres (si los hay) itinerarios que minimizan el numero
-    // de cambios de avión entre esos dos aeropuertos
+  def itinerariosEscalas(vuelos: List[Vuelo], aeropuertos: List[Aeropuerto]) : (String,String) => List[Itinerario] = {
+    // Calcula el numero total de escalas en un itinerario, para cada uno se suman las escalas y se añaden el
+    // numero de cambios de avión, que es igual al tamaño de los itinerarios menos 1
+    def contarEscalas(itinerario: Itinerario): Int = {
+      itinerario.map(_.Esc).sum + itinerario.size
+    }
 
-    (a: String, b: String) => List()
+    //Itinerarios, metodo de busqueda
+    def itinerarios(codigo1: String, codigo2: String): List[Itinerario] = {
+      def vuelosDesde(codigo: String): List[Vuelo] =
+        vuelos.filter(v => v.Org == codigo)
+
+      def itinerariosDesdeVuelo(vuelo: Vuelo, destino: String): List[Itinerario] =
+        if (vuelo.Dst == destino) List(List(vuelo))
+        else vuelosDesde(vuelo.Dst).flatMap(v => itinerariosDesdeVuelo(v, destino).map(vuelo :: _))
+
+      vuelosDesde(codigo1).flatMap(v => itinerariosDesdeVuelo(v, codigo2)).take(3)
+    }
+
+    // Retornar los itinerarios ordenados en base al numero de escalas calculado
+    (codigo1: String, codigo2: String) => itinerarios(codigo1, codigo2).sortBy(contarEscalas)
   }
 
   def itinerariosAire(
