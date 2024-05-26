2cd8909e14670c2b2f8baec1278026df7c8d0228
Elkin Angulo <elkinsamir643@gmail.com>
2024-05-26 16:37:21 -0500

    Creando y Optimizando las funciones paralelas del paquete de itinerarios

--- src/main/scala/ItinerariosPar/package.scala
+++ src/main/scala/ItinerariosPar/package.scala
@@ -4,70 +4,119 @@ import scala.collection.parallel.CollectionConverters._
 import Datos.Aeropuerto
 import Datos.Itinerario
 import Datos.Vuelo
+import Itinerarios.obtenerGMT
 
 package object ItinerariosPar {
 
-  def itinerariosPar(
-      vuelos: List[Vuelo],
-      aeropuertos: List[Aeropuerto]
-  ): (String, String) => List[Itinerario] = {
-    // Recibe vuelos, una lista de todos los vuelos disponibles y
-    // aeropuertos, una lista de todos los aeropuertos
-    // y devuelve una función que recibe c1 y c2, dos códigos de aeropuerto
-    // y devuelve todos los itinerarios entre esos dos aeropuertos
+  def obtenerTiempoEsperaPar( aeropuertos: List[Aeropuerto], itinerario: Itinerario,acc: Int ): Int = {
+    itinerario match {
+      case Nil | _ :: Nil => acc
+      case vuelo1 :: vuelo2 :: tail => {
+        val (v1DstGMT, v2OrgGMT) = parallel(obtenerGMT(aeropuertos, vuelo1.Dst), obtenerGMT(aeropuertos, vuelo2.Org))
 
-    (a: String, b: String) => List()
+        val HLv1GMT = if (vuelo1.HL - v1DstGMT < 0) (vuelo1.HL - v1DstGMT + 24) else (vuelo1.HL - v1DstGMT)
+        val HSv2GMT = if (vuelo2.HS - v2OrgGMT < 0) (vuelo2.HS - v2OrgGMT + 24) else (vuelo2.HS - v2OrgGMT)
+
+        val diferenciaHvGMT = (HSv2GMT * 60 + vuelo2.MS) - (HLv1GMT * 60 + vuelo1.ML)
+        
+        obtenerTiempoEsperaPar(aeropuertos, vuelo2 :: tail, acc + diferenciaHvGMT)
+      }
+    }
   }
 
-  def itinerariosTiempoPar(
-      vuelos: List[Vuelo],
-      aeropuertos: List[Aeropuerto]
-  ): (String, String) => List[Itinerario] = {
-    // Recibe vuelos, una lista de todos los vuelos disponibles y
-    // aeropuertos, una lista de todos los aeropuertos
-    // y devuelve una función que recibe c1 y c2, códigos de aeropuerto
-    // y devuelve una función que devuelve los tres (si los hay) itinerarios que minimizan el
-    // tiempo de viaje entre esos dos aeropuertos
-
-    (a: String, b: String) => List()
+  def obtenerTiempoVueloPar( aeropuertos: List[Aeropuerto], itinerario: Itinerario ): Int = {    
+    itinerario.foldRight(0)((vuelo, acc) => {
+      val (vOrgGMT, vDstGMT) = parallel(obtenerGMT(aeropuertos, vuelo.Org), obtenerGMT(aeropuertos, vuelo.Dst))
+      val HSvGMT = if (vuelo.HS - vOrgGMT < 0) (vuelo.HS - vOrgGMT + 24) else (vuelo.HS - vOrgGMT)
+      val HLvGMT = if (vuelo.HL - vDstGMT < 0) (vuelo.HL - vDstGMT + 24) else (vuelo.HL - vDstGMT)
+      val diferenciaHvGMT = ((HLvGMT * 60 + vuelo.ML) - (HSvGMT * 60 + vuelo.MS))
+      if ( diferenciaHvGMT < 0 ) acc +  diferenciaHvGMT + (24 * 60) else acc + diferenciaHvGMT
+    })
   }
 
-  def itinerariosEscalasPar(
-      vuelos: List[Vuelo],
-      aeropuertos: List[Aeropuerto]
-  ): (String, String) => List[Itinerario] = {
-    // Recibe vuelos, una lista de todos los vuelos disponibles y
-    // aeropuertos, una lista de todos los aeropuertos
-    // y devuelve una función que recibe c1 y c2, códigos de aeropuerto
-    // y devuelve los tres (si los hay) itinerarios que minimizan el numero
-    // de cambios de avión entre esos dos aeropuertos
+  def itinerariosPar( vuelos: List[Vuelo],aeropuertos: List[Aeropuerto] ): (String, String) => List[Itinerario] = {
+    (a: String, b: String) => {
 
-    (a: String, b: String) => List()
+      val vuelosPorOrigen = vuelos.groupBy(_.Org)
+
+      def aeropuetoVisitado( cod: String, vuelosVisitados: Set[Vuelo] ): Boolean = {
+        vuelosVisitados.exists(vuelo => vuelo.Org == cod || vuelo.Dst == cod)
+      }
+
+      def buscarItinerarios(cod1: String, cod2: String, vuelosVisitados: Set[Vuelo]): List[Itinerario] = {
+        if (cod1 == cod2) List(List())
+        else {
+          vuelosPorOrigen.getOrElse(cod1, List())
+          .filter(vuelo => !vuelosVisitados.contains(vuelo) && !aeropuetoVisitado(vuelo.Dst, vuelosVisitados))
+          .map( vuelo => 
+            task(buscarItinerarios(vuelo.Dst, cod2, vuelosVisitados + vuelo).map(itinerario => vuelo :: itinerario)) 
+          ).flatMap(_.join())      
+        }  
+      }
+      buscarItinerarios(a, b, Set())
+    }
   }
 
-  def itinerariosAirePar(
-      vuelos: List[Vuelo],
-      aeropuertos: List[Aeropuerto]
-  ): (String, String) => List[Itinerario] = {
-    // Recibe vuelos, una lista de todos los vuelos disponibles y
-    // aeropuertos, una lista de todos los aeropuertos
-    // y devuelve una función que recibe c1 y c2, códigos de aeropuerto
-    // y devuelve los tres (si los hay) itinerarios que minimizan
-    // el tiempo en el aire entre esos dos aeropuertos
+  def itinerariosTiempoPar(vuelos: List[Vuelo], aeropuertos: List[Aeropuerto]): (String, String) => List[Itinerario] = {
+    (a: String, b: String) => {
 
-    (a: String, b: String) => List()
+      // Función que obtiene el tiempo total de un itinerario
+      def obtenerTiempoTotal(itinerario: Itinerario): Int = {
+        val (tiempoVuelo, tiempoEspera) = parallel(obtenerTiempoVueloPar(aeropuertos, itinerario), obtenerTiempoEsperaPar(aeropuertos, itinerario, 0))
+        tiempoVuelo + tiempoEspera
+      }
+
+      // Obtener todos los itinerarios entre los aeropuertos a y b
+      val todosItinerarios = itinerariosPar(vuelos, aeropuertos)(a, b)
+
+      // Mapear cada itinerario a su tiempo total en paralelo
+      todosItinerarios.map(itinerario => task((itinerario, obtenerTiempoTotal(itinerario))))
+                      .map(_.join()).sortBy(_._2).take(3).map(_._1)
+    }
   }
 
-  def itinerarioSalidaPar(
-      vuelos: List[Vuelo],
-      aeropuerto: List[Aeropuerto]
-  ): (String, String, Int, Int) => Itinerario = {
-    // Recibe vuelos, una lista de todos los vuelos disponibles y
-    // aeropuertos, una lista de todos los aeropuertos
-    // y devuelve una función que recibe c1 y c2, códigos de aeropuerto, y h:m una hora de la cita en c2
-    // y devuelve el itinerario que optimiza la hora de salida para llegar a tiempo a la cita
+  def itinerariosEscalasPar(vuelos: List[Vuelo], aeropuertos: List[Aeropuerto]): (String, String) => List[Itinerario] = {
+    (a: String, b: String) => {
 
-    (a: String, b: String, h: Int, m: Int) => List()
+      // Obtener todos los itinerarios entre los aeropuertos a y b
+      val todosItinerarios = itinerariosPar(vuelos, aeropuertos)(a, b)
+
+      def calcularEscalasTotales(itinerario: Itinerario): Int = {
+        itinerario.map(_.Esc).sum + (itinerario.length - 1)
+      }
+
+      // Mapear cada itinerario a su número total de escalas en paralelo
+      todosItinerarios.map(itinerario => task((itinerario, calcularEscalasTotales(itinerario))))
+                      .map(_.join()).sortBy(_._2).take(3).map(_._1)
+
+    }
+  }
+
+
+  def itinerariosAirePar(vuelos: List[Vuelo], aeropuertos: List[Aeropuerto]): (String, String) => List[Itinerario] = {
+    (a: String, b: String) => {
+      val todosItinerarios = itinerariosPar(vuelos, aeropuertos)(a, b)
+
+      todosItinerarios.map { itinerario => task((itinerario, obtenerTiempoVueloPar(aeropuertos, itinerario))) }
+                      .map(_.join()).sortBy(_._2).take(3).map(_._1)
+    }
+  }
+
+  def itinerarioSalidaPar(vuelos: List[Vuelo], aeropuertos: List[Aeropuerto]): (String, String, Int, Int) => Itinerario = {
+    (cod1: String, cod2: String, h: Int, m: Int) => {
+
+      //Obtener todos los itinerarios entre los aeropuertos cod1 y cod2 para convertir en una lista paralela para el filtro
+      val todosItinerarios = itinerariosPar(vuelos, aeropuertos)(cod1, cod2).par
+
+      val posiblesResultados = todosItinerarios.filter(itinerario => {
+          val vDstGMT = obtenerGMT(aeropuertos, itinerario.last.Dst)
+          (itinerario.last.HL - vDstGMT) * 60 + itinerario.last.ML <= ((h - vDstGMT) * 60 + m)
+      }).par
+
+      // Encontrar el itinerario con la hora de salida más temprana en paralelo
+      if (posiblesResultados.nonEmpty) posiblesResultados.minBy(itinerario => itinerario.maxBy(_.HS).HS) else List()
+      
+    }
   }
 
 }
